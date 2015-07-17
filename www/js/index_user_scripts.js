8823aae233ed6357cc93734b342797fcbff9aa07
yosep <yosman_18@hotmail.com>
2015-07-17 16:13:13 -0500

    primerasubida

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,19 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* listitem  Cusco-V alle Sagrado */
+    $(document).on("click", ".uib_w_61", function(evt)
+    {
+        /* your code goes here */ 
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
